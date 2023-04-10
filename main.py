592844ff4a336626ebf689dea9cca4fbbc8faff8
StrivingToAdoniss <110491831+StrivingToAdoniss@users.noreply.github.com>
2023-04-10 17:43:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from Model.User import user
 from Model.Category import categories
 from Model.User_Answer import user_answer
 from admins import admins_list
+import pandas as pd
+from sklearn.cluster import KMeans
 
 logging.basicConfig(level=logging.INFO)
 
@@ -18,6 +20,63 @@ order = {}
 data = questions.get_data()
 
 print(data)
+
+async def group_users_by_personality(message: types.Message) -> None:
+    # Перевіряємо рівень доступу
+    if str(message.from_user.id) not in admins_list:
+        await message.answer("No access.")
+        return
+
+    # Взаємодіємо з ДБ
+    database = Database()
+
+  
+    user_answer = UserAnswer(database)
+
+  
+    user_data = user_answer.get_all_data()
+
+    # Отримуємо відповіді
+    answer_columns = user_data.columns[1:]
+
+    
+    kmeans_model = KMeans(n_clusters=2, random_state=42).fit(user_data[answer_columns])
+
+    # Призначаєом кластер для юзерів
+    cluster_assignments = kmeans_model.predict(user_data[answer_columns])
+
+    # Мапимо кластери
+    labels = {0: 'Introverted', 1: 'Extroverted'}
+    user_data['label'] = [labels[c] for c in cluster_assignments]
+
+    # Сепаруємо юзерів або в "Extroverted" або в "Introverted"
+    introverted_users = user_data[user_data['label'] == 'Introverted']
+    extroverted_users = user_data[user_data['label'] == 'Extroverted']
+
+    # Групуємо по 4 юзера "Extroverted"
+    extroverted_groups: List[Tuple[str, pd.DataFrame]] = []
+    for i in range(0, len(extroverted_users), 4):
+        extroverted_group = extroverted_users.iloc[i:i+4]
+        extroverted_group_name = f'Extroverted{i//4 + 1}'
+        extroverted_groups.append((extroverted_group_name, extroverted_group))
+
+    # Групуємо по 4 юзера в "Introverted"
+    introverted_groups: List[Tuple[str, pd.DataFrame]] = []
+    for i in range(0, len(introverted_users), 4):
+        introverted_group = introverted_users.iloc[i:i+4]
+        introverted_group_name = f'Introverted{i//4 + 1}'
+        introverted_groups.append((introverted_group_name, introverted_group))
+
+    # Прінтимо в консольку(Треба придумати норм спосіб)
+    result = 'Introverted Groups:\n'
+    for group_name, group in introverted_groups:
+        result += f"{group_name}\n{group.to_string(index=False)}\n\n"
+
+    result += 'Extroverted Groups:\n'
+    for group_name, group in extroverted_groups:
+        result += f"{group_name}\n{group.to_string(index=False)}\n\n"
+
+    await message.answer(result)
 
 
 # Хендлер на команду /start
