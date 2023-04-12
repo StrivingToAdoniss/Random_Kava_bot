840c2ba55252810d200d69cc8c0d561b6e6e2cdd
StrivingToAdoniss <110491831+StrivingToAdoniss@users.noreply.github.com>
2023-04-13 00:19:20 +0300

    Update User_Answer.py

--- Model/User_Answer.py
+++ Model/User_Answer.py
@@ -5,43 +5,51 @@ class UserAnswer:
     def __init__(self):
         self.database = database
 
-
-
     def get_data_user(self, user_id):
         """
         Gets all data from User_Answer database
         :return: data from User_Answer database
         """
-        self.database.cursor.execute(f"SELECT * FROM User_Answer where id_user = '{user_id}'")
+        self.database.cursor.execute(
+            f"SELECT * FROM User_Answer where id_user = '{user_id}'"
+        )
         return self.database.cursor.fetchall()
 
- def get_data_user_all_questions(self, user_id):
+    def get_data_user_all_questions(self):
         """
-        Gets all data from User_Answer database for users who have answered all questions
+        Gets data from User_Answer database for users who have answered all the questions
         :return: data from User_Answer database
         """
-        self.database.cursor.execute(f"SELECT * FROM User_Answer WHERE id_user = '{user_id}' "
-                                      "GROUP BY id_user HAVING COUNT(DISTINCT id_question) = (SELECT COUNT(*) FROM Question)")
-        return self.database.cursor.fetchall()
+        self.database.cursor.execute("SELECT DISTINCT id_user FROM User_Answer")
+        users = self.database.cursor.fetchall()
 
-    def get_user_answers_data(self, user_id):
-        user_answers_data = []
-        user_answers_data.append(user_id)
+        all_questions_answered_users = []
+        for user in users:
+            user_id = user[0]
+            self.database.cursor.execute(
+                f"SELECT COUNT(DISTINCT id_question) FROM User_Answer WHERE id_user = '{user_id}'"
+            )
+            num_questions_answered = self.database.cursor.fetchone()[0]
+            self.database.cursor.execute("SELECT COUNT(DISTINCT id) FROM Question")
+            total_num_questions = self.database.cursor.fetchone()[0]
+            if num_questions_answered == total_num_questions:
+                self.database.cursor.execute(
+                    f"SELECT * FROM User_Answer WHERE id_user = '{user_id}'"
+                )
+                user_answers = self.database.cursor.fetchall()
+                all_questions_answered_users.extend(user_answers)
 
-        user_answers = self.get_data_user_all_questions(user_id)
-        for answer in user_answers:
-            user_answers_data.append(answer[3])
-
-        return user_answers_data
+        return all_questions_answered_users
 
     def print(self, user_id):
-
         user_answer_str = ""
         for answer in self.get_data_user(user_id):
             # print(answer[0])
-            self.database.cursor.execute(f"SELECT title FROM Answer where id = {answer[3]}")
+            self.database.cursor.execute(
+                f"SELECT title FROM Answer where id = {answer[3]}"
+            )
             answer_name = self.database.cursor.fetchone()
-            user_answer_str+=f"{answer_name[0]}\n"
+            user_answer_str += f"{answer_name[0]}\n"
         return user_answer_str
 
     def insert_data(self, id_question, id_user, id_answer):
@@ -49,16 +57,26 @@ class UserAnswer:
         Inserts data in User_Answer database
         :param data: data in the format id, id_question, id_user
         """
-        self.database.cursor.execute(f"SELECT * FROM User_Answer WHERE id_question = {id_question} and id_user = '{id_user}'")
+        self.database.cursor.execute(
+            f"SELECT * FROM User_Answer WHERE id_question = {id_question} and id_user = '{id_user}'"
+        )
         id_user_answer = self.database.cursor.fetchone()
         if id_user_answer is None:
-            print("INSERT INTO User_Answer(id_question, id_user, id_answer) "
-                                         f"VALUES({id_question}, '{id_user}', {id_answer})")
-            self.database.cursor.execute("INSERT INTO User_Answer (id_question, id_user, id_answer)"
-                                         f"VALUES({id_question}, '{id_user}', {id_answer})")
+            print(
+                "INSERT INTO User_Answer(id_question, id_user, id_answer) "
+                f"VALUES({id_question}, '{id_user}', {id_answer})"
+            )
+            self.database.cursor.execute(
+                "INSERT INTO User_Answer (id_question, id_user, id_answer)"
+                f"VALUES({id_question}, '{id_user}', {id_answer})"
+            )
         else:
-            print(f"UPDATE User_Answer SET id_answer = {id_answer} WHERE id = {id_user_answer[0]}")
-            self.database.cursor.execute(f"UPDATE User_Answer SET id_answer = {id_answer} WHERE id = {id_user_answer[0]}")
+            print(
+                f"UPDATE User_Answer SET id_answer = {id_answer} WHERE id = {id_user_answer[0]}"
+            )
+            self.database.cursor.execute(
+                f"UPDATE User_Answer SET id_answer = {id_answer} WHERE id = {id_user_answer[0]}"
+            )
 
         self.database.connection.commit()
 
@@ -67,14 +85,18 @@ class UserAnswer:
         Updates id_question in User_Answer database
         :param data: data in the format id, id_question
         """
-        self.database.cursor.execute(f"UPDATE User_Answer SET id_question = '{data[1]}' WHERE id = {data[0]}")
+        self.database.cursor.execute(
+            f"UPDATE User_Answer SET id_question = '{data[1]}' WHERE id = {data[0]}"
+        )
 
     def updateUser(self, *data):
         """
         Updates id_user in User_Answer database
         :param data: data in the format id, id_user
         """
-        self.database.cursor.execute(f"UPDATE User_Answer SET id_user = '{data[1]}' WHERE id = {data[0]}")
+        self.database.cursor.execute(
+            f"UPDATE User_Answer SET id_user = '{data[1]}' WHERE id = {data[0]}"
+        )
         self.database.connection.commit()
 
 
